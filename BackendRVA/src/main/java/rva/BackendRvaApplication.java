dc9066951141dfa0df6b82cada6948747cf0b98d
Milica <Milica@DESKTOP-RI90GMU>
2020-03-18 17:17:54 +0100

    Drugi čas

--- BackendRVA/src/main/java/rva/BackendRvaApplication.java
+++ BackendRVA/src/main/java/rva/BackendRvaApplication.java
@@ -1,7 +1,10 @@
 package rva;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendRvaApplication {
@@ -10,4 +13,14 @@ public class BackendRvaApplication {
 		SpringApplication.run(BackendRvaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context){
+		return args -> {
+			System.out.println("Beans provided by Spring framework");
+			String[] beanNames = context.getBeanDefinitionNames();
+			for(String beanName: beanNames){
+				System.out.println(beanName);
+			}
+		};
+	}
 }
